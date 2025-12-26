a95b49a69a7969ef449d2d2cfb2c32efc9453065
lednicazar <35409468+lednicazar@users.noreply.github.com>
2025-12-26 17:28:45 +0100

    Update cover.py

--- components/bus_t4/cover.py
+++ components/bus_t4/cover.py
@@ -1,40 +1,34 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import cover
-from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL, CONF_USE_ADDRESS
+from esphome.components import cover, uart
+from esphome.const import CONF_ADDRESS, CONF_ID, CONF_USE_ADDRESS
 
 DEPENDENCIES = ["uart"]
-
 AUTO_LOAD = ["cover"]
 
+bus_t4_ns = cg.esphome_ns.namespace("bus_t4")
+Nice = bus_t4_ns.class_("NiceBusT4", cover.Cover, cg.Component, uart.UARTDevice)
 
-bus_t4_ns = cg.esphome_ns.namespace('bus_t4')
-Nice = bus_t4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
+CONFIG_SCHEMA = (
+    cover.COVER_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(Nice),
+            cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
+            cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
+        }
+    )
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
-CONFIG_SCHEMA = cover._COVER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(Nice),
-    cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
-    cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
-#    cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
-}).extend(cv.COMPONENT_SCHEMA)
-
-
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-
-    yield cover.register_cover(var, config)
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+    await cover.register_cover(var, config)
 
     if CONF_ADDRESS in config:
-        address = config[CONF_ADDRESS]
-        cg.add(var.set_to_address(address))
+        cg.add(var.set_to_address(config[CONF_ADDRESS]))
 
     if CONF_USE_ADDRESS in config:
-        use_address = config[CONF_USE_ADDRESS]
-        cg.add(var.set_from_address(use_address))
-        
-        
- #   if CONF_UPDATE_INTERVAL in config:
- #       update_interval = config[CONF_UPDATE_INTERVAL]
- #       cg.add(var.set_update_interval(update_interval))
-
+        cg.add(var.set_from_address(config[CONF_USE_ADDRESS]))
